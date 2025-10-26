11dfea10db56e3183de32a69265f816cc4f11618
Sam Hanes <sam@maltera.com>
2025-10-25 20:05:20 -0700

    Add home-manager and some dev tools.

--- flake.nix
+++ flake.nix
@@ -6,13 +6,24 @@
       url = "github:nix-community/lanzaboote/v0.4.2";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nixpkgs, lanzaboote, ... }: {
+  outputs = inputs@{ self, nixpkgs, lanzaboote, home-manager, ... }: {
     nixosConfigurations.selkie = nixpkgs.lib.nixosSystem {
       modules = [
         lanzaboote.nixosModules.lanzaboote
         ./hosts/selkie
+
+	home-manager.nixosModules.home-manager {
+	  home-manager.useGlobalPkgs = true;
+	  home-manager.useUserPackages = true;
+	  home-manager.users.maia = import ./home;
+	}
       ];
     };
   };
