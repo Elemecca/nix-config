56398443377eb92a174904fa477730e607245c46
Sam Hanes <sam@maltera.com>
2026-04-04 16:25:25 -0700

    Enable firewall and local name resolution.

--- modules/system.nix
+++ modules/system.nix
@@ -7,6 +7,28 @@
   time.timeZone = "America/Los_Angeles";
   i18n.defaultLocale = "en_US.UTF-8";
 
+  networking.firewall.enable = true;
+  networking.nftables.enable = true;
+  networking.firewall.allowedUDPPorts = [ 5353 ];
+
+  services.resolved.enable = true;
+#  services.resolved = {
+#    enable = true;
+#    settings = {
+#      DNS = [
+#        "1.1.1.1#one.one.one.one"
+#        "1.0.0.1#one.one.one.one"
+#        "2606:4700:4700::1111#one.one.one.one"
+#        "2606:4700:4700::1001#one.one.one.one"
+#      ];
+#      Domains = [ "~." ];
+#      DNSOverTLS = "true";
+#
+#      LLMNR = "resolve";
+#      MulticastDNS = "resolve";
+#    };
+#  };
+
   environment.systemPackages = with pkgs; [
     file
     htop
